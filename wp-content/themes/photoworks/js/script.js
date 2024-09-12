607e9b183ef24366cbc9b392feace0159c920e31
Aurelie Boisson <aurel195@yahoo.fr>
2024-09-12 16:03:24 +0200

    etape2

--- wp-content/themes/photoworks/js/script.js
+++ wp-content/themes/photoworks/js/script.js
@@ -8,12 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const openModalButton = document.querySelector("#contact-menu-item a");
     const modal = document.getElementById("myModal");
     const modalOverlay = document.querySelector(".modal_overlay");
+    let modalAnim = document.querySelector('.modal');
+    modal.classList.add('fade-in-out');
+
+    
 
     function openModal() {
         modal.style.display = "block";
+        modalAnim.classList.add('fade-in-out');
     }    
     function closeModal() {
         modal.style.display = "none";
+        modalAnim.classList.remove('fade-in-out');
     }
     
     openModalButton.addEventListener("click",openModal);
